78830f749d268105f9a45fa83af85818cdef6a7a
Ross McFadyen <ross.mcfadyen@digirati.com>
2017-05-19 20:16:04 +0100

    Added code to push latest docker tag

--- docker/makeDockerfiles.sh
+++ docker/makeDockerfiles.sh
@@ -26,13 +26,13 @@ function buildDockerFile {
   docker exec -i mysql mysql -uroot  <<< "create database IF NOT EXISTS omeka_test;"
 
   docker images
-  
+
   echo -e "\033[00;32m ===> Spinning up a container running ${2} and attempting to run unit tests \033[0m\n";
   docker run -i -t --link mysql:mysql ${PACKAGE_NAME}:${2} /bin/sh -c "sed -i 's/^host.*/host = "mysql"/' application/test/config/database.ini && sed -i 's/^user.*/user = "root"/' application/test/config/database.ini && sed -i 's/^dbname.*/dbname = "omeka_test"/' application/test/config/database.ini &&./node_modules/gulp/bin/gulp.js test:php"
-  
+
   echo -e "\033[00;32m ===> Dropping previous database\033[0m\n";
   docker exec -i mysql mysql -uroot  <<< "drop database omeka_test;"
-  
+
   if [ $? -ne 0 ]; then
    echo -e "\033[00;31m ERROR: Unit tests didn't pass, we won't we pushing this image\033[0m\n";
   else
@@ -40,7 +40,7 @@ function buildDockerFile {
     docker push ${DOCKER_USER}/${PACKAGE_NAME}:${2}
     cd -;
     echo -e "\033[00;32m====================S=U=C=C=E=S=S=======================\033[0m\n";
-  fi 
+  fi
 }
 
 echo -e "\033[00;32m ===> Creating database container for running tests\033[0m\n";
@@ -54,6 +54,15 @@ do
    # wait for 5 seconds before check again
    sleep 5
 done
+
+function pushLatest {
+     echo -e "\033[00;32m========================================================";
+     echo -e "Pushing PHP 7.1 FPM with the tag latest";
+     echo -e "========================================================\033[0m";
+
+     docker tag omeka-s:7.1-fpm ${DOCKER_USER}/${PACKAGE_NAME}:latest
+     docker push ${DOCKER_USER}/${PACKAGE_NAME}:latest
+}
 
 for php in "${PHP_VERSIONS[@]}"
 do
@@ -77,3 +86,6 @@ do
 
   done
 done
+
+pushLatest
+
