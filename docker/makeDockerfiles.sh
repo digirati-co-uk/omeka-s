fe9dfc6499a21ca0956293c0047021babb2b3506
Stephen Fraser <stephenwf@users.noreply.github.com>
2017-06-13 15:30:41 +0100

    Create makeDockerfiles.sh

--- docker/makeDockerfiles.sh
+++ docker/makeDockerfiles.sh
@@ -46,12 +46,12 @@ function buildDockerFile {
     if [[ "$TRAVIS_BRANCH" = "develop" ]] && [[ "$TRAVIS_PULL_REQUEST" = "false" ]]; then
       echo -e "\033[00;32m ===> Pushing to Dockerhub\033[0m\n";
       docker push ${DOCKER_USER}/${PACKAGE_NAME}:${2}
-      cd -;
       echo -e "\033[00;32m====================S=U=C=C=E=S=S=======================\033[0m\n";
     else
       echo -e "\033[00;31m Unit tests passed but we're not pushing this as its a PR or a feature branch\033[0m\n";
     fi
-  fi
+  fi # This needs to always happen, see line 21
+  cd -;
 }
 
 echo -e "\033[00;32m ===> Creating database container for running tests\033[0m\n";
@@ -117,7 +117,7 @@ pushLatest
     echo -e "\033[00;32m ===> Pushing this change downstream to the NLW omeka build \033[0m\n";
 
     curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token ${TRAVIS_ACCESS_TOKEN}" -d "$body" https://api.travis-ci.com/repo/digirati-co-uk%2Fnlw-omeka-build/requests
-    
+
     echo -e "\033[00;32m ===> Pushing this change downstream to the Digirati omeka dev build \033[0m\n";
 
     curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token ${TRAVIS_ACCESS_TOKEN}" -d "$body" https://api.travis-ci.com/repo/digirati-co-uk%2Fomeka-s-dev/requests
